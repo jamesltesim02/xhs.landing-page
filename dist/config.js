3445f86fc783c242745e33d60745f0b4a070203d
a a <aa@ip-192-168-1-6.ap-southeast-1.compute.internal>
2023-05-19 17:12:55 +0800

    init

---
+++ dist/config.js
@@ -0,0 +1,20 @@
+window.HuarsConfig = {
+  /** potato客服地址 */
+  POTATO_CHAT: 'https://ptcc.in/joinchat/MHIa1GrQrvTNDafXDJx8Vw',
+  /** Telegram客服地址 */
+  TELEGRAM_CHAT: 'https://t.me/xiaohuangshu_business',
+  /** iOS下载地址 */
+  DOWNLOAD_IOS: 'https://statapi.putaoxiehui.com/ios/yaochi_v2/ch/0/light.mobileconfig',
+  /** Android下载地址 */
+  DOWNLOAD_ANDROID: 'https://apk.ycjklads.com/yc_v2.5.0_stable20221027110523.apk',
+  /** 留言地址 */
+  LEAVE_MESSAGE: 'https://t.me/xiaohuangshu_business',
+  /** 官网显示内容 */
+  SITE_TITLE: 'www.6fffc.com',
+  /** 官网地址 */
+  SITE_URL: 'https://www.6fffc.com',
+  /** 商务合作显示内容 */
+  BUSINESS_TITLE: 'telegarm/小飞机：xiaohuangshu_business',
+  /** 商务合作显示内容 */
+  BUSINESS_URL: 'https://t.me/xiaohuangshu_business',
+}
