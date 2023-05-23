e39fb4600028f63f2c00359e551523638009e925
a a <aa@ip-192-168-1-6.ap-southeast-1.compute.internal>
2023-05-23 12:09:45 +0800

    修改安卓下载地址

--- dist/config.js
+++ dist/config.js
@@ -6,7 +6,7 @@ window.HuarsConfig = {
   /** iOS下载地址 */
   DOWNLOAD_IOS: 'https://statapi.putaoxiehui.com/ios/yaochi_v2/ch/0/light.mobileconfig',
   /** Android下载地址 */
-  DOWNLOAD_ANDROID: 'https://apk.ycjklads.com/yc_v2.5.0_stable20221027110523.apk',
+  DOWNLOAD_ANDROID: 'https://apk.6fffc.com/app-release.2.3.9.apk',
   /** 留言地址 */
   LEAVE_MESSAGE: 'https://t.me/xiaohuangshu_business',
   /** 官网显示内容 */
